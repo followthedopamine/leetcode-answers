659b538290471f1b88223c5ef2e313e85a01051b
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-09-08 07:26:44 +1200

    Set up tests for sortedSquares

---
+++ squares-of-a-sorted-array/sortedSquares.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/squares-of-a-sorted-array/
+
+// Follow up: Squaring each element and sorting the new array is very trivial, could you find an O(n) solution using a different approach?
+
+// Initial thoughts: create array of all the negatives squared, then when we get to the positives create a new array and test if the number from the negative fits in;
+
+/**
+ *
+ * @param {number[]} nums
+ * @returns {number[]}
+ */
+
+const sortedSquares = (nums) => {};
