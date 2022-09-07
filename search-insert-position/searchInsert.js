155a95619920a83e60beb44c8f48025f4c82a068
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-09-08 06:49:25 +1200

    Solved search-insert-position

--- search-insert-position/searchInsert.js
+++ search-insert-position/searchInsert.js
@@ -1,3 +1,29 @@
-const searchInsert = (nums, target) => {};
+// https://leetcode.com/problems/search-insert-position/
+
+/**
+ *
+ * @param {number[]} nums
+ * @param {number} target
+ * @returns {number}
+ */
+
+const searchInsert = (nums, target) => {
+  let left = 0;
+  let middle = 0;
+  let right = nums.length - 1;
+  if (nums[left] > target) return left;
+  if (nums[right] < target) return right + 1;
+  while (left <= right) {
+    middle = Math.floor((left + right) / 2);
+    if (nums[middle] === target) return middle;
+    if (left + 1 == right) return right;
+    if (nums[middle] < target) {
+      left = middle;
+    }
+    if (nums[middle] > target) {
+      right = middle;
+    }
+  }
+};
 
 module.exports = searchInsert;
