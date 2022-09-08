340c735fe8ab23a2d33c7f1fefeaa870eeb8dd2d
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-09-09 03:58:51 +1200

    Solved two-sum-ii-input-array-is-sorted

--- two-sum-ii-input-array-is-sorted/twoSum.js
+++ two-sum-ii-input-array-is-sorted/twoSum.js
@@ -1,5 +1,22 @@
 "use strict";
+/*Runtime: 87 ms, faster than 82.22% of TypeScript online submissions for Two Sum II - Input Array Is Sorted.
+Memory Usage: 43.8 MB, less than 62.81% of TypeScript online submissions for Two Sum II - Input Array Is Sorted.*/
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (numbers, target) => {
-    return [];
+const twoSum = (numbers, target) => {
+    let left = 0;
+    let right = numbers.length - 1;
+    while (left < right) {
+        let current = numbers[left] + numbers[right];
+        if (current === target) {
+            return [left + 1, right + 1];
+        }
+        if (current < target) {
+            left++;
+        }
+        if (current > target) {
+            right--;
+        }
+    }
+    return [-1, -1];
 };
+exports.default = twoSum;
--- two-sum-ii-input-array-is-sorted/twoSum.ts
+++ two-sum-ii-input-array-is-sorted/twoSum.ts
@@ -1,3 +1,23 @@
-export default (numbers: number[], target: number): number[] => {
-    return [];
+ /*Runtime: 87 ms, faster than 82.22% of TypeScript online submissions for Two Sum II - Input Array Is Sorted.
+Memory Usage: 43.8 MB, less than 62.81% of TypeScript online submissions for Two Sum II - Input Array Is Sorted.*/
+ 
+ const twoSum = (numbers: number[], target: number): number[] => {
+    let left: number = 0;
+    let right: number = numbers.length - 1;
+
+    while(left < right) {
+        let current: number = numbers[left] + numbers[right];
+        if(current === target) {
+            return [left + 1, right + 1];
+        }
+        if(current < target) {
+            left++;
+        }
+        if(current > target) {
+            right--;
+        }
+    }
+    return [-1, -1]
 }
+
+export default twoSum;
