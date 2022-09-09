c865dfc23da7f1b4b2d3bd3eff77f8d0e15381ff
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-09-10 03:29:14 +1200

    Solved reverse-words-in-a-string-iii

--- reverse-words-in-a-string-iii/reverseWords.js
+++ reverse-words-in-a-string-iii/reverseWords.js
@@ -1,7 +1,24 @@
 "use strict";
 // https://leetcode.com/problems/reverse-words-in-a-string-iii/
 Object.defineProperty(exports, "__esModule", { value: true });
+const reverseString = (str) => {
+    let sArr = str.split("");
+    let left = 0;
+    let right = str.length - 1;
+    while (left < right) {
+        [sArr[left], sArr[right]] = [sArr[right], sArr[left]];
+        left++;
+        right--;
+    }
+    return sArr.join("");
+};
+// Not sure if it's a typescript thing but I couldn't get this to work modifying strArr in place with either map or a for of loop.
 const reverseWords = (s) => {
-    return '';
+    let strArr = s.split(" ");
+    let result = [];
+    for (let str of strArr) {
+        result.push(reverseString(str));
+    }
+    return result.join(" ");
 };
 exports.default = reverseWords;
--- reverse-words-in-a-string-iii/reverseWords.ts
+++ reverse-words-in-a-string-iii/reverseWords.ts
@@ -1,7 +1,25 @@
 // https://leetcode.com/problems/reverse-words-in-a-string-iii/
 
+const reverseString = (str: string) => {
+    let sArr: string[] = str.split("");
+    let left: number = 0;
+    let right: number = str.length - 1;
+    while(left < right) {
+        [sArr[left], sArr[right]] = [sArr[right], sArr[left]];
+        left++;
+        right--;
+    }
+    return sArr.join("");
+}
+
+// Not sure if it's a typescript thing but I couldn't get this to work modifying strArr in place with either map or a for of loop.
 const reverseWords = (s: string): string => {
-    return '';
+    let strArr: string[] = s.split(" ");
+    let result: string[] = [];
+    for(let str of strArr) {
+        result.push(reverseString(str));
+    }
+    return result.join(" ");
 };
 
 export default reverseWords;
