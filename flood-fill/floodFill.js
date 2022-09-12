babd7223fb7a24e319fccdc168aa4793ac2413ea
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-09-13 00:54:34 +1200

    Solved flood-fill

--- flood-fill/floodFill.js
+++ flood-fill/floodFill.js
@@ -1,7 +1,24 @@
 "use strict";
 // https://leetcode.com/problems/flood-fill/
 Object.defineProperty(exports, "__esModule", { value: true });
+const floodFillRC = (image, sr, sc, color, originalColor) => {
+    if (image[sr][sc] === originalColor) {
+        image[sr][sc] = color;
+        if (sr - 1 >= 0)
+            floodFillRC(image, sr - 1, sc, color, originalColor); // Top
+        if (sr + 1 <= image.length - 1)
+            floodFillRC(image, sr + 1, sc, color, originalColor); // Bottom
+        if (sc - 1 >= 0)
+            floodFillRC(image, sr, sc - 1, color, originalColor); // Left
+        if (sc + 1 <= image[0].length - 1)
+            floodFillRC(image, sr, sc + 1, color, originalColor); // Right
+    }
+};
 const floodFill = (image, sr, sc, color) => {
-    return [[0]];
+    if (image[sr][sc] !== color) {
+        floodFillRC(image, sr, sc, color, image[sr][sc]);
+        image[sr][sc] = color;
+    }
+    return image;
 };
 exports.default = floodFill;
--- flood-fill/floodFill.ts
+++ flood-fill/floodFill.ts
@@ -1,12 +1,34 @@
 // https://leetcode.com/problems/flood-fill/
 
+const floodFillRC = (
+  image: number[][],
+  sr: number,
+  sc: number,
+  color: number,
+  originalColor: number
+): void => {
+  if (image[sr][sc] === originalColor) {
+    image[sr][sc] = color;
+    if (sr - 1 >= 0) floodFillRC(image, sr - 1, sc, color, originalColor); // Top
+    if (sr + 1 <= image.length - 1)
+      floodFillRC(image, sr + 1, sc, color, originalColor); // Bottom
+    if (sc - 1 >= 0) floodFillRC(image, sr, sc - 1, color, originalColor); // Left
+    if (sc + 1 <= image[0].length - 1)
+      floodFillRC(image, sr, sc + 1, color, originalColor); // Right
+  }
+};
+
 const floodFill = (
   image: number[][],
   sr: number,
   sc: number,
   color: number
 ): number[][] => {
-  return [[0]];
+  if (image[sr][sc] !== color) {
+    floodFillRC(image, sr, sc, color, image[sr][sc]);
+    image[sr][sc] = color;
+  }
+  return image;
 };
 
 export default floodFill;
