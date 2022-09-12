6b7eba018106d61cc1e18ceac08c3cf4b10e89c0
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-09-13 06:47:43 +1200

    Solved substrings-of-size-three-with-distinct-characters

--- substrings-of-size-three-with-distinct-characters/countGoodSubstrings.ts
+++ substrings-of-size-three-with-distinct-characters/countGoodSubstrings.ts
@@ -1,7 +1,28 @@
 // https://leetcode.com/problems/substrings-of-size-three-with-distinct-characters/
-
+// TODO: Fix repeating code
 const countGoodSubstrings = (s: string): number => {
-  return 0;
+  let map: any = {};
+  let goodSubstrings = 0;
+  const goodSubstringLength = 3;
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    map[char] = (map[char] || 0) + 1;
+    if (i === goodSubstringLength - 1) {
+      if (!Object.values(map).some((value) => value !== 1 && value !== 0)) {
+        goodSubstrings++;
+      }
+    }
+
+    if (i >= goodSubstringLength) {
+      const prevChar = s[i - goodSubstringLength];
+      map[prevChar] = map[prevChar] - 1;
+      if (!Object.values(map).some((value) => value !== 1 && value !== 0)) {
+        goodSubstrings++;
+      }
+    }
+  }
+  return goodSubstrings;
 };
 
 export default countGoodSubstrings;
