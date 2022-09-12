ad4a15663b5afceb27b2c01fd0f9e368ad92a9fa
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-09-13 02:06:41 +1200

    solved max-area-of-island

--- max-area-of-island/maxAreaOfIsland.js
+++ max-area-of-island/maxAreaOfIsland.js
@@ -1,7 +1,40 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 // https://leetcode.com/problems/max-area-of-island/
-const maxAreaOfIsland = (grid) => {
-    return 0;
+var _a;
+Object.defineProperty(exports, "__esModule", { value: true });
+/*Runtime: 107 ms, faster than 84.32% of TypeScript online submissions for Max Area of Island.
+Memory Usage: 46.1 MB, less than 57.63% of TypeScript online submissions for Max Area of Island.*/
+class Island {
+}
+_a = Island;
+Island.grid = [];
+Island.getIslandSize = (r, c) => {
+    const island = 1;
+    if (r < 0 ||
+        c < 0 ||
+        r >= _a.grid.length ||
+        c >= _a.grid[0].length ||
+        _a.grid[r][c] !== island)
+        return 0;
+    _a.grid[r][c] = 0;
+    return (1 +
+        _a.getIslandSize(r + 1, c) +
+        _a.getIslandSize(r - 1, c) +
+        _a.getIslandSize(r, c + 1) +
+        _a.getIslandSize(r, c - 1));
 };
+Island.maxAreaOfIsland = (grid) => {
+    const island = 1;
+    let maxIsland = 0;
+    _a.grid = grid;
+    for (let r = 0; r < grid.length; r++) {
+        for (let c = 0; c < grid[0].length; c++) {
+            if (grid[r][c] === island) {
+                maxIsland = Math.max(maxIsland, _a.getIslandSize(r, c));
+            }
+        }
+    }
+    return maxIsland;
+};
+const maxAreaOfIsland = Island.maxAreaOfIsland;
 exports.default = maxAreaOfIsland;
--- max-area-of-island/maxAreaOfIsland.ts
+++ max-area-of-island/maxAreaOfIsland.ts
@@ -1,6 +1,46 @@
 // https://leetcode.com/problems/max-area-of-island/
-const maxAreaOfIsland = (grid: number[][]): number => {
-  return 0;
-};
 
+/*Runtime: 107 ms, faster than 84.32% of TypeScript online submissions for Max Area of Island.
+Memory Usage: 46.1 MB, less than 57.63% of TypeScript online submissions for Max Area of Island.*/
+
+class Island {
+  private static grid: number[][] = [];
+
+  private static getIslandSize = (r: number, c: number): number => {
+    const island = 1;
+    if (
+      r < 0 ||
+      c < 0 ||
+      r >= this.grid.length ||
+      c >= this.grid[0].length ||
+      this.grid[r][c] !== island
+    )
+      return 0;
+
+    this.grid[r][c] = 0;
+
+    return (
+      1 +
+      this.getIslandSize(r + 1, c) +
+      this.getIslandSize(r - 1, c) +
+      this.getIslandSize(r, c + 1) +
+      this.getIslandSize(r, c - 1)
+    );
+  };
+
+  public static maxAreaOfIsland = (grid: number[][]): number => {
+    const island = 1;
+    let maxIsland = 0;
+    this.grid = grid;
+    for (let r = 0; r < grid.length; r++) {
+      for (let c = 0; c < grid[0].length; c++) {
+        if (grid[r][c] === island) {
+          maxIsland = Math.max(maxIsland, this.getIslandSize(r, c));
+        }
+      }
+    }
+    return maxIsland;
+  };
+}
+const maxAreaOfIsland = Island.maxAreaOfIsland;
 export default maxAreaOfIsland;
