804d87f417d54b71afeeecc13ebabb4657f66f03
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-09-15 05:25:12 +1200

    Solved 01-matrix

--- 01-matrix/updateMatrix.js
+++ 01-matrix/updateMatrix.js
@@ -1,6 +1,28 @@
 "use strict";
+// https://leetcode.com/problems/01-matrix
 Object.defineProperty(exports, "__esModule", { value: true });
-const updateMatrix = (mat) => {
-    return [[0]];
+const updateMatrix = (matrix) => {
+    let longestPath = matrix.length + matrix[0].length - 2;
+    const rows = matrix.length;
+    const cols = matrix[0].length;
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            if (matrix[row][col] === 0)
+                continue;
+            matrix[row][col] = Math.min(matrix[row - 1] ? matrix[row - 1][col] + 1 : longestPath, matrix[row][col - 1] !== undefined
+                ? matrix[row][col - 1] + 1
+                : longestPath);
+        }
+    }
+    for (let row = rows - 1; row >= 0; row--) {
+        for (let col = cols - 1; col >= 0; col--) {
+            if (matrix[row][col] === 0)
+                continue;
+            matrix[row][col] = Math.min(matrix[row][col], matrix[row + 1] ? matrix[row + 1][col] + 1 : longestPath, matrix[row][col + 1] !== undefined
+                ? matrix[row][col + 1] + 1
+                : longestPath);
+        }
+    }
+    return matrix;
 };
 exports.default = updateMatrix;
--- 01-matrix/updateMatrix.ts
+++ 01-matrix/updateMatrix.ts
@@ -1,5 +1,36 @@
-const updateMatrix = (mat: number[][]): number[][] => {
-  return [[0]];
+// https://leetcode.com/problems/01-matrix
+
+const updateMatrix = (matrix: number[][]): number[][] => {
+  let longestPath = matrix.length + matrix[0].length - 2;
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      if (matrix[row][col] === 0) continue;
+
+      matrix[row][col] = Math.min(
+        matrix[row - 1] ? matrix[row - 1][col] + 1 : longestPath,
+        matrix[row][col - 1] !== undefined
+          ? matrix[row][col - 1] + 1
+          : longestPath
+      );
+    }
+  }
+
+  for (let row = rows - 1; row >= 0; row--) {
+    for (let col = cols - 1; col >= 0; col--) {
+      if (matrix[row][col] === 0) continue;
+
+      matrix[row][col] = Math.min(
+        matrix[row][col],
+        matrix[row + 1] ? matrix[row + 1][col] + 1 : longestPath,
+        matrix[row][col + 1] !== undefined
+          ? matrix[row][col + 1] + 1
+          : longestPath
+      );
+    }
+  }
+  return matrix;
 };
 
 export default updateMatrix;
