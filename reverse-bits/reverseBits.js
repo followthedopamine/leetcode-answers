5c654bdb8a6a643182f7d2a439990858a7064ba0
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-09-18 07:00:49 +1200

    Set up tests for reverse-bits

---
+++ reverse-bits/reverseBits.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// https://leetcode.com/problems/reverse-bits
+const reverseBits = (n) => {
+    return 0;
+};
+exports.default = reverseBits;
---
+++ reverse-bits/reverseBits.ts
@@ -0,0 +1,6 @@
+// https://leetcode.com/problems/reverse-bits
+const reverseBits = (n: number): number => {
+  return 0;
+};
+
+export default reverseBits;
