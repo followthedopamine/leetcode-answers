92f5a8eb630fe7e637e480d812a89b4a5659130a
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-09-18 06:20:36 +1200

    Solved triangle

--- triangle/minimumTotal.js
+++ triangle/minimumTotal.js
@@ -2,6 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 // https://leetcode.com/problems/triangle
 const minimumTotal = (triangle) => {
-    return 0;
+    var _a, _b;
+    for (let i = 1; i < triangle.length; i++) {
+        const elementsLength = triangle[i].length;
+        for (let j = 0; j < elementsLength; j++) {
+            const leftValue = (_a = triangle[i - 1][j - 1]) !== null && _a !== void 0 ? _a : Infinity;
+            const rightValue = (_b = triangle[i - 1][j]) !== null && _b !== void 0 ? _b : Infinity;
+            triangle[i][j] += leftValue < rightValue ? leftValue : rightValue;
+        }
+    }
+    return triangle[triangle.length - 1].reduce((acc, sum) => acc < sum ? acc : sum);
 };
 exports.default = minimumTotal;
--- triangle/minimumTotal.ts
+++ triangle/minimumTotal.ts
@@ -1,6 +1,16 @@
 // https://leetcode.com/problems/triangle
 const minimumTotal = (triangle: number[][]): number => {
-  return 0;
+  for (let i = 1; i < triangle.length; i++) {
+    const elementsLength: number = triangle[i].length;
+    for (let j = 0; j < elementsLength; j++) {
+      const leftValue: number = triangle[i - 1][j - 1] ?? Infinity;
+      const rightValue: number = triangle[i - 1][j] ?? Infinity;
+      triangle[i][j] += leftValue < rightValue ? leftValue : rightValue;
+    }
+  }
+  return triangle[triangle.length - 1].reduce((acc, sum) =>
+    acc < sum ? acc : sum
+  );
 };
 
 export default minimumTotal;
