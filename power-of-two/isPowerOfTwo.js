c19ca27c9798180f1c9d5c6d7aea8d3558348c6a
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-09-19 02:41:19 +1200

    Solved power-of-two

--- power-of-two/isPowerOfTwo.js
+++ power-of-two/isPowerOfTwo.js
@@ -2,6 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 // https://leetcode.com/problems/power-of-two
 const isPowerOfTwo = (n) => {
-    return true;
+    if (n === 1)
+        return true;
+    while (n > 1) {
+        if (n === 2)
+            return true;
+        if (n % 2 === 0) {
+            n = n / 2;
+        }
+        else {
+            return false;
+        }
+    }
+    return false;
 };
 exports.default = isPowerOfTwo;
--- power-of-two/isPowerOfTwo.ts
+++ power-of-two/isPowerOfTwo.ts
@@ -1,6 +1,15 @@
 // https://leetcode.com/problems/power-of-two
 const isPowerOfTwo = (n: number): boolean => {
-  return true;
+  if (n === 1) return true;
+  while (n > 1) {
+    if (n === 2) return true;
+    if (n % 2 === 0) {
+      n = n / 2;
+    } else {
+      return false;
+    }
+  }
+  return false;
 };
 
 export default isPowerOfTwo;
