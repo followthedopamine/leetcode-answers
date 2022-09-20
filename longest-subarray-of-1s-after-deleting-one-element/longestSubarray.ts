492257002af525ee2e1df2f605e5ec80a0541368
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-09-21 00:12:08 +1200

    Solved longest-subarray-of-1s-after-deleting-one-element

--- longest-subarray-of-1s-after-deleting-one-element/longestSubarray.ts
+++ longest-subarray-of-1s-after-deleting-one-element/longestSubarray.ts
@@ -1,6 +1,23 @@
 // https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/
+/* Runtime: 76 ms, faster than 90.00% of TypeScript online submissions for Longest Subarray of 1's After Deleting One Element.
+Memory Usage: 47.9 MB, less than 60.00% of TypeScript online submissions for Longest Subarray of 1's After Deleting One Element. */
 const longestSubarray = (nums: number[]): number => {
-  return 0;
+  let last = 0;
+  let count = 0;
+  let result = 0;
+  for (let i = 0; i <= nums.length; i++) {
+    if (nums[i] === 1) count++;
+    else {
+      // ^ TODO: if (nums[i] === 0) doesn't work here... wtf
+      if (last + count > result) {
+        result = last + count;
+      }
+      last = count;
+      count = 0;
+    }
+  }
+  if (result === nums.length) result--;
+  return result;
 };
 
 export default longestSubarray;
--- tests/longestSubarrayTests.ts
+++ tests/longestSubarrayTests.ts
@@ -11,4 +11,7 @@ describe("longestSubarray", () => {
   it("Should return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.", () => {
     assert.equal(longestSubarray([1, 1, 1]), 2);
   });
+  it("Should return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.", () => {
+    assert.equal(longestSubarray([1, 1, 0, 0, 1, 1, 1, 0, 1]), 4);
+  });
 });
