bae47bb74eaa8582710ab9cc56e9a22218151da4
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-11-01 07:56:02 +1300

    Set up tests for valid-palindrome

---
+++ valid-palindrome/isPalindrome.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using std::cout;
+using std::string;
+
+class Solution {
+ public:
+  bool isPalindrome(string s) {
+    return false;
+  }
+};
+
+int main() {
+  Solution s;
+  cout << s.isPalindrome("test");
+}
