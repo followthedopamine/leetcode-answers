18951b859f4525f57907e457fa1a2201d01a36e1
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-11-01 08:32:26 +1300

    Solved valid-palindrome

--- valid-palindrome/isPalindrome.cpp
+++ valid-palindrome/isPalindrome.cpp
@@ -1,3 +1,6 @@
+// https://leetcode.com/problems/valid-palindrome/
+
+#include <cctype>
 #include <iostream>
 #include <string>
 using std::cout;
@@ -6,11 +9,29 @@ using std::string;
 class Solution {
  public:
   bool isPalindrome(string s) {
-    return false;
+    int left = 0;
+    int right = s.length();
+    while (left < right) {
+      if (!isalpha(s[left]) && !isdigit(s[left])) {
+        left++;
+        continue;
+      }
+      if (!isalpha(s[right]) && !isdigit(s[right])) {
+        right--;
+        continue;
+      }
+      if (tolower(s[left]) != tolower(s[right])) return false;
+      left++;
+      right--;
+    }
+    return true;
   }
 };
 
 int main() {
   Solution s;
-  cout << s.isPalindrome("test");
+  cout << s.isPalindrome("A man, a plan, a canal: Panama") << " ";  // true
+  cout << s.isPalindrome("race a car") << " ";                      // false
+  cout << s.isPalindrome(" ") << " ";                               // true
+  cout << s.isPalindrome("0P");                                     // false
 }
